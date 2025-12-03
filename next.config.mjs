e40a475d2a6edc3f7d2ac92148906de85b7b0a3e
kapilkaranmathur <kapilkaranmathur@gmail.com>
2025-12-03 15:13:38 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'bcrypt'],
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcrypt'],
+  },
 };
 
 export default nextConfig;
